66d99101630d6b9614b1a0abb6ebfe0b6f894377
Marcus Hellberg <marcus@vaadin.com>
2018-11-30 11:09:11 -0600

    code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,51 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="component">
+    <div class="form" @keyup.enter="addPerson">
+      <vaadin-text-field label="First Name" v-model="currentPerson.firstName"></vaadin-text-field>
+      <vaadin-text-field label="Last Name" v-model="currentPerson.lastName"></vaadin-text-field>
+      <vaadin-button @click="addPerson">Add</vaadin-button>
+    </div>
+    <vaadin-grid :items.prop="people">
+      <vaadin-grid-column path="firstName" header="First name"></vaadin-grid-column>
+      <vaadin-grid-column path="lastName" header="Last name"></vaadin-grid-column>
+    </vaadin-grid>
   </div>
 </template>
 
 <script>
+import "@vaadin/vaadin-button";
+import "@vaadin/vaadin-grid";
+import "@vaadin/vaadin-text-field";
+
+const getPerson = () => {
+  return {
+    firstName: "",
+    lastName: ""
+  };
+};
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data: function() {
+    return {
+      people: [{ firstName: "foo", lastName: "bars" }],
+      currentPerson: getPerson()
+    };
+  },
+  methods: {
+    addPerson: function() {
+      this.people = [...this.people, this.currentPerson];
+      this.currentPerson = getPerson();
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.form {
+  margin-bottom: 16px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.form * {
+  margin-right: 4px;
 }
 </style>
